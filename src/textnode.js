7fba1e04f48d9cfde5032c0a883ea62df651824e
Marek Fojtl <marek.fojtl@firma.seznam.cz>
2013-04-05 10:07:06 +0200

    #1 fixes

--- src/textnode.js
+++ src/textnode.js
@@ -6,31 +6,30 @@ JAX.TextNode = JAK.ClassMaker.makeClass({
 
 JAX.TextNode.prototype.$constructor = function(node) {
 	if (!("nodeType" in node) || node.nodeType != 3) { throw new Error("JAX.TextNode constructor accepts only HTML node as its parameter. See doc for more information.") }
-	this._node = node;
 	this.NODE = node;
 };
 
 JAX.TextNode.prototype.appendTo = function(node) {
 	var node = node instanceof JAX.HTMLElm ? node.NODE : node;
-	node.appendChild(this._node);
+	node.appendChild(this.NODE);
 	return this;
 };
 
 JAX.TextNode.prototype.appendBefore = function(node) {
 	var node = node instanceof JAX.HTMLElm ? node.NODE : node;
-	node.parentNode.insertBefore(this._node, node);
+	node.parentNode.insertBefore(this.NODE, node);
 	return this;
 };
 
 JAX.TextNode.prototype.removeFromDOM = function() {
 	try {
-		this._node.parentNode.removeChild(this._node);
+		this.NODE.parentNode.removeChild(this.NODE);
 	} catch(e) {};
 	return this;
 };
 
 JAX.TextNode.prototype.getParent = function() {
-	if (this._node.parentNode) { return new JAX.HTMLElm(this._node.parentNode); }
+	if (this.NODE.parentNode) { return new JAX.HTMLElm(this.NODE.parentNode); }
 	return null;
 };
 
