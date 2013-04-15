f82ad25b61150d6fd72d9a179324ceb842a79641
Marek Fojtl <marek.fojtl@firma.seznam.cz>
2013-04-15 14:00:51 +0200

    #1 fixes, removed node array

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ cat core.js >> ../lib/jax.js
 cat nodehtml.js >> ../lib/jax.js
 cat nodetext.js >> ../lib/jax.js
 cat nodedoc.js >> ../lib/jax.js
-cat nodearray.js >> ../lib/jax.js
 cat animation.js >> ../lib/jax.js
 
 echo "if (!window.JAX) { window.JAX = JAX; }" >> ../lib/jax.js
