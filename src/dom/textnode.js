c9359ca6e411abad3ce05bb0c31d9d8807464407
Marek Fojtl <marek.fojtl@firma.seznam.cz>
2014-02-02 20:11:12 +0100

    #1 doc

--- src/dom/textnode.js
+++ src/dom/textnode.js
@@ -9,12 +9,17 @@
  * @class JAX.TextNode
  */
 JAX.TextNode = JAK.ClassMaker.makeClass({
-	NAME: "JAX.Node",
+	NAME: "JAX.TextNode",
 	VERSION: "1.0",
 	EXTEND: JAX.Node,
 	IMPLEMENT: [JAX.IMoveableNode]
 });
 
+/** 
+ * @constructor
+ *
+ * @param {object} node objekt typu window.Text
+ */
 JAX.TextNode.prototype.$constructor = function(node) {
 	this.$super(node);
 
@@ -24,6 +29,12 @@ JAX.TextNode.prototype.$constructor = function(node) {
 	this.isRemoveable = true;
 };
 
+/**
+ * @method nastaví nebo vrátí textovou hodnotu uzlu
+ *
+ * @param {string || undefined} text textový řetězec
+ * @returns {object || string} JAX.Node
+ */
 JAX.TextNode.prototype.text = function(text) {
 	if (!arguments.length) { 
 		return this._node.nodeValue;
@@ -34,11 +45,22 @@ JAX.TextNode.prototype.text = function(text) {
 	return this;
 };
 
+/**
+ * @method nastaví textovou hodnotu na prázdný řetězec
+ *
+ * @returns {object} JAX.Node
+ */
 JAX.TextNode.prototype.clear = function() {
 	this._node.nodeValue = "";
 	return this;
 };
 
+/**
+ * @method porovná sama sebe se zadaným parametrem. Pokud se jedná o stejný node, vrátí true.
+ *
+ * @param {object} node DOM uzel nebo instance JAX.Node
+ * @returns {boolean}
+ */
 JAX.TextNode.prototype.eq = function(node) {
 	if (!node) { return false; }
 	var jaxElm = node instanceof JAX.Node ? node : JAX(node);
